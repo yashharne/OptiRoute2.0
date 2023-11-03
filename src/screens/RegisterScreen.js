0cb4e7b4dfa7ef3402f3b288c6f22819e3b0326f
yashharne <yashharne.ed@gmail.com>
2023-11-04 00:23:17 +0530

    button changes

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -72,6 +72,7 @@ export default function RegisterScreen({ navigation }) {
         mode="contained"
         onPress={onSignUpPressed}
         style={{ marginTop: 24 }}
+        textcolor={"#ffffff"}
       >
         Sign Up
       </Button>
--- src/screens/ResetPasswordScreen.js
+++ src/screens/ResetPasswordScreen.js
@@ -1,34 +1,35 @@
-import React, { useState } from 'react'
-import Background from '../components/Background'
-import BackButton from '../components/BackButton'
-import Logo from '../components/Logo'
-import Header from '../components/Header'
-import TextInput from '../components/TextInput'
-import Button from '../components/Button'
-import { emailValidator } from '../helpers/emailValidator'
+import React, { useState } from "react";
+import Background from "../components/Background";
+import BackButton from "../components/BackButton";
+import Logo from "../components/Logo";
+import Header from "../components/Header";
+import TextInput from "../components/TextInput";
+import Button from "../components/Button";
+import { emailValidator } from "../helpers/emailValidator";
 
 export default function ResetPasswordScreen({ navigation }) {
-  const [email, setEmail] = useState({ value: '', error: '' })
+  const [email, setEmail] = useState({ value: "", error: "" });
 
   const sendResetPasswordEmail = () => {
-    const emailError = emailValidator(email.value)
+    const emailError = emailValidator(email.value);
     if (emailError) {
-      setEmail({ ...email, error: emailError })
-      return
+      setEmail({ ...email, error: emailError });
+      return;
     }
-    navigation.navigate('LoginScreen')
-  }
+    navigation.navigate("LoginScreen");
+  };
 
   return (
     <Background>
       <BackButton goBack={navigation.goBack} />
       <Logo />
+      <Header textcolor="#09bdab">OPTIROUTE</Header>
       <Header>Restore Password</Header>
       <TextInput
         label="E-mail address"
         returnKeyType="done"
         value={email.value}
-        onChangeText={(text) => setEmail({ value: text, error: '' })}
+        onChangeText={(text) => setEmail({ value: text, error: "" })}
         error={!!email.error}
         errorText={email.error}
         autoCapitalize="none"
@@ -41,9 +42,10 @@ export default function ResetPasswordScreen({ navigation }) {
         mode="contained"
         onPress={sendResetPasswordEmail}
         style={{ marginTop: 16 }}
+        textcolor={"#ffffff"}
       >
         Send Instructions
       </Button>
     </Background>
-  )
+  );
 }
