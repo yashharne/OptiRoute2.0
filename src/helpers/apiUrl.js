73c5a53667f236611212df69000c74186e84cf3a
yashharne <yashharne.ed@gmail.com>
2025-04-15 03:25:19 +0530

    shopping plan screen working

--- src/helpers/apiUrl.js
+++ src/helpers/apiUrl.js
@@ -1,3 +1,3 @@
-//  export const apiUrl = "https://optiroute-backend-19xg.onrender.com";
-export const apiUrl =
-  "https://134b-2409-40c2-505c-3abc-fce1-1e05-3a22-44e3.ngrok-free.app";
+export const apiUrl = "https://optiroute-backend-19xg.onrender.com";
+// export const apiUrl =
+//   "https://134b-2409-40c2-505c-3abc-fce1-1e05-3a22-44e3.ngrok-free.app";
--- src/screens/PlanScreen.js
+++ src/screens/PlanScreen.js
@@ -1,16 +1,18 @@
 // screens/PlanScreen.js
 import React from "react";
-import { View, Text, FlatList, StyleSheet } from "react-native";
+import { View, Text, FlatList, StyleSheet, Dimensions } from "react-native";
 import Button from "../components/Button";
 import Background from "../components/Background";
 import Header from "../components/Header";
+
+const screenWidth = Dimensions.get("window").width;
 
 export default function PlanScreen({ route, navigation }) {
   const { plan } = route.params;
 
   return (
     <Background>
-      <Header>Shopping Plan</Header>
+      <Header style={styles.headerContainer}>Shopping Plan</Header>
       <FlatList
         data={Object.entries(plan)}
         keyExtractor={([shop]) => shop}
@@ -21,7 +23,7 @@ export default function PlanScreen({ route, navigation }) {
               <Text style={styles.shopName}>{shop}</Text>
               {items.map((entry, idx) => (
                 <Text key={idx} style={styles.itemLine}>
-                  • {entry.item}: ₹{entry.cost}
+                  • {entry.item} ₹ {entry.cost}
                 </Text>
               ))}
             </View>
@@ -43,18 +45,33 @@ export default function PlanScreen({ route, navigation }) {
 
 const styles = StyleSheet.create({
   shopBlock: {
+    width: screenWidth * 0.8,
     backgroundColor: "#f0f0f0",
     padding: 10,
     marginVertical: 8,
     borderRadius: 10,
   },
   shopName: {
-    fontSize: 18,
+    fontSize: 20,
+    color: "#333",
+    textAlign: "center",
     fontWeight: "bold",
-    marginBottom: 5,
+    marginBottom: 10,
   },
   itemLine: {
-    fontSize: 16,
+    fontSize: 18,
     marginLeft: 10,
+  },
+  headerContainer: {
+    marginTop: 30,
+    marginBottom: 20,
+    fontSize: 26,
+    backgroundColor: "#d4d9c1",
+    padding: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#686e4f",
   },
 });
